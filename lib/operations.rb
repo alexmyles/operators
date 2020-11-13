f219a7f486d40ca57b48246357a1fecc11797b16
alex <alex16myles@gmail.com>
2020-11-13 14:40:22 -0600

    commit tree a4fdaabab09cd42c49ee69d97a558c85ef0dc8cd

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,18 @@
 def unsafe?(speed)
-
+    if speed>= 40 && speed<= 60
+        false
+    else
+        true
+ end
 end
-
 
 
 def not_safe?(speed)
-	
+    if speed>= 40 && speed<= 60? true : false
+        false
+    else
+        true
+	end
 end
 	
-
 
